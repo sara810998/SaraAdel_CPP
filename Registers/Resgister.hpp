f83283491ecf5e07e7ab6082740150fe8780a3e9
LAPTOP-8LUL3EEN\sara <saraadelelsayed33@gmail.com>
2022-03-09 03:34:48 +0200

    Error handeling added

--- Registers/Resgister.hpp
+++ Registers/Resgister.hpp
@@ -10,6 +10,13 @@ typedef enum{
     RESERVED
 }Enum_BitPermission;
 
+/*
+ *each bit in write variable represent the write permission of this bit in the register 1---write 0---no_write
+ *each bit in read variable represent the read permission of this bit in the register 1---read 0---no_read
+ *Example: 8 bit register with bit configuration       .... RW W R RES W RW R RES .... 
+ *so the struct values will be assigned as follow: Write 0b 1  1 0  0  1  1 0  0 
+ *                                               : Read  0b 1  0 1  0  0  1 1  0  
+ */
 typedef struct{
    Template_RegisterSize Write;
    Template_RegisterSize Read;
@@ -32,29 +39,76 @@ class Register{
 
         }
         Enum_ErrorStatus setRegisterValue(Template_RegisterSize RegisterValue){
-            (*ptr_RegisterAddress) = RegisterValue;
 
+             Enum_ErrorStatus Error= OK;
+            if(this->RegisterPermission.Write == (Template_RegisterSize)0XFFFFFFFFFFFFFFFF){
+
+                (*ptr_RegisterAddress) = RegisterValue;
+
+            }else{
+                Error=NOT_OK;
+            }
+            return Error;
+            
         }
 
         Enum_ErrorStatus getRegisterValue(Template_RegisterSize &RegisterValue){
-            RegisterValue = (*ptr_RegisterAddress);
+
+             Enum_ErrorStatus Error= OK;
+
+            if(this->RegisterPermission.Read == (Template_RegisterSize)0XFFFFFFFFFFFFFFFF){
+
+                RegisterValue = (*ptr_RegisterAddress);
+            }else{
+               Error=NOT_OK; 
+            }
+            return Error;
         }
 
         Enum_ErrorStatus setBit(uint8_t BitNumber){
 
-            (*ptr_RegisterAddress) |= (1<< BitNumber);
+            Enum_ErrorStatus Error= OK;
+            bool permission;
+            permission = (this->RegisterPermission.Write) & (1<< BitNumber);
+            if(permission){
+                 (*ptr_RegisterAddress) |= (1<< BitNumber);
+            }else{
+                Error=NOT_OK; 
+            }
+            return Error;
         }
 
         Enum_ErrorStatus clearBit(uint8_t BitNumber){
 
-            (*ptr_RegisterAddress) &= (~(1<< BitNumber));
+            Enum_ErrorStatus Error= OK;
+            bool permission;
+            permission = (this->RegisterPermission.Write) & (1<< BitNumber);
+            if(permission){
+                (*ptr_RegisterAddress) &= (~(1<< BitNumber));
+            }else{
+                Error=NOT_OK; 
+            }
+            return Error;
+
         }
 
         Enum_ErrorStatus getBit(uint8_t BitNumber,bool Value){ 
-             Value =  (*ptr_RegisterAddress) & (1<< BitNumber);
+
+            Enum_ErrorStatus Error= OK;
+            bool permission;
+            permission = (this->RegisterPermission.Read) & (1<< BitNumber);
+            if(permission){
+                 Value =  (*ptr_RegisterAddress) & (1<< BitNumber);
+            }else{
+                Error=NOT_OK; 
+            }
+            return Error;
+            
         }
 
-        Enum_ErrorStatus setPermission(uint8_t BitNumber,Enum_BitPermission Enum_BitPermission){ 
+        Enum_ErrorStatus setPermission(uint8_t BitNumber,Enum_BitPermission Enum_BitPermission){
+
+            Enum_ErrorStatus Error= OK; 
             switch(Enum_BitPermission):
                 case WRITE_ONLY:
                     (this->RegisterPermission.Write) |= (1<< BitNumber);
@@ -72,13 +126,15 @@ class Register{
                     (this->RegisterPermission.Write) &= (~(1<< BitNumber));
                     (this->RegisterPermission.Read) &= (~(1<< BitNumber));
                     break;
-
+            return Error;
         }
 
         Enum_ErrorStatus setPermission(Struct_RegisterPermission* RegisterPermission){ 
 
+            Enum_ErrorStatus Error= OK;
             this->RegisterPermission.Write = RegisterPermission->Write;
             this->RegisterPermission.Read = RegisterPermission->Read;
+            return Error;
         }
   
 
